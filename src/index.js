0d28b1b91028957d763c80605da794e111a5088a
Matthew <matthew7172@gmail.com>
2020-12-28 02:46:09 +0100

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import App from "./App"
-import {Provider} from "react-redux"
-import store from "./store"
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-
-ReactDOM.render( <BrowserRouter > <Provider store={store}> <App /> </Provider> </BrowserRouter>, document.querySelector("#root"))
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from "redux"
-import GameList from "./GameList"
-import GameInfo from "./GameInfo"
-
-export default combineReducers({
-    GameList,
-    GameInfo,
-})
